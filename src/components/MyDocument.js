0d728fa5de199695a6bda5589f781639e8aca8e9
Victor Sanchez <victor_sanchez@outlook.com>
2022-06-07 01:24:16 -0300

    adding logo and text

--- src/components/MyDocument.js
+++ src/components/MyDocument.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import { Page, Document, Text, StyleSheet, PDFViewer } from '@react-pdf/renderer';
+import { Page, Document, Text, View, StyleSheet, PDFViewer } from '@react-pdf/renderer';
 import Table from './Table';
+import Logo from './Logo';
 
 export default function MyDocument() {
 
     // Create styles
     const styles = StyleSheet.create({
-    page: {
-        backgroundColor: 'white',
-        color: "black",
-        flexDirection: "column",
-        margin: 10,
-    },
-    title:{
-        color: 'green', 
-        textAlign: 'center',
-        margin: 5,
-    },
-    viewer: {
-        width: window.screen.width,
-        height: window.screen.height
-      },
+        viewer: {
+            width: window.screen.width,
+            height: window.screen.height
+         },
+        page: {
+            backgroundColor: 'white',
+            color: "black",
+            flexDirection: "column",
+            padding: 20
+        },
+        title:{
+            color: 'green', 
+            textAlign: 'center',
+            margin: 5,
+        },
+        headerSection: {
+            flexDirection: "row",
+            alignItems: "center",
+        },
+        text:{
+            fontSize: 11,
+            textAlign: "justify",
+            margin: 10,
+            color: 'black',
+        }
     });
 
     return (
@@ -31,8 +42,12 @@ export default function MyDocument() {
                       size={"A4"} 
                       style={styles.page}
                 >
-                    <Text style={styles.title}>THIS IS A TEST</Text>
+                    <View style={styles.headerSection}>
+                    <Logo />
+                    <Text style={styles.title}>THIS IS A TEST USING REACT-PDF</Text>
+                    </View>
                     <Table />
+                    <Text style={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, enim nostrum, adipisci dolor officia iure hic laboriosam atque fuga perferendis est fugit ullam repellat eaque. Ex eaque tempora assumenda eos! Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ex vitae veritatis error necessitatibus consectetur dolore, dicta reiciendis officia sit molestiae quisquam quos exercitationem minus ab tempore cumque, enim facilis!</Text>
                 </Page>
             </Document>
         </PDFViewer>
